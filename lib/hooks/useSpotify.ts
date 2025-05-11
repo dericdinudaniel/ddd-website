1172736fefd649bdcc80d5ed420aed1ea9670e8c
Deric Dinu Daniel <dericdd@umich.edu>
2025-05-11 20:59:41 +0530

    add fallback spotify data

--- lib/hooks/useSpotify.ts
+++ lib/hooks/useSpotify.ts
@@ -1,9 +1,79 @@
 import useSWR from "swr";
+import {
+  fallbackNowPlaying,
+  fallbackTracks,
+  fallbackArtists,
+} from "@/lib/spotify-fallback";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+type SpotifyTrack = {
+  title: string;
+  songUrl: string;
+  albumImageUrl: string;
+  artists: Array<{
+    name: string;
+    url: string;
+  }>;
+};
 
-export const useNowPlaying = () => useSWR("/api/spotify/now-playing", fetcher);
+type SpotifyArtist = {
+  name: string;
+  imageUrl: string;
+  artistUrl: string;
+};
 
-export const useTopTracks = () => useSWR("/api/spotify/top-tracks", fetcher);
+type NowPlaying = {
+  isPlaying: boolean;
+  title: string;
+  artist: string;
+  album: string;
+  albumImageUrl: string;
+  songUrl: string;
+};
 
-export const useTopArtists = () => useSWR("/api/spotify/top-artists", fetcher);
+const fetcher = async (url: string) => {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      return null;
+    }
+    return res.json();
+  } catch {
+    return null;
+  }
+};
+
+export const useNowPlaying = () => {
+  const { data, error, isLoading } = useSWR<NowPlaying>(
+    "/api/spotify/now-playing",
+    fetcher
+  );
+  return {
+    data: data || fallbackNowPlaying,
+    isLoading,
+    isError: error,
+  };
+};
+
+export const useTopTracks = () => {
+  const { data, error, isLoading } = useSWR<SpotifyTrack[]>(
+    "/api/spotify/top-tracks",
+    fetcher
+  );
+  return {
+    data: data || fallbackTracks,
+    isLoading,
+    isError: error,
+  };
+};
+
+export const useTopArtists = () => {
+  const { data, error, isLoading } = useSWR<SpotifyArtist[]>(
+    "/api/spotify/top-artists",
+    fetcher
+  );
+  return {
+    data: data || fallbackArtists,
+    isLoading,
+    isError: error,
+  };
+};
---
+++ lib/spotify-fallback.ts
@@ -0,0 +1,243 @@
+export const fallbackTracks = [
+  {
+    title: "Giving Up (feat. Jaden Michaels)",
+    songUrl: "https://open.spotify.com/track/07FuFHGBF6nbBk4KHrT1jt",
+    albumImageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02fea0745b3c5884d696bc1f7d",
+    artists: [
+      {
+        name: "QUIX",
+        url: "https://open.spotify.com/artist/19EW4WBhl0fvZUQgi7wV5M",
+      },
+      {
+        name: "Jaden Michaels",
+        url: "https://open.spotify.com/artist/4OKeMjg0BPykaJfAbr3dxs",
+      },
+    ],
+  },
+  {
+    title: "ON FIRE",
+    songUrl: "https://open.spotify.com/track/09HjLlsBh3iqLEkOgH4Ac4",
+    albumImageUrl:
+      "https://i.scdn.co/image/ab67616d00001e020c4d41a83e9571974b491397",
+    artists: [
+      {
+        name: "JOYRYDE",
+        url: "https://open.spotify.com/artist/24neLwyYRyj4ItaGnFeIT0",
+      },
+    ],
+  },
+  {
+    title: "In My Head",
+    songUrl: "https://open.spotify.com/track/6touT68ZnV9zwgYabqj2eK",
+    albumImageUrl:
+      "https://i.scdn.co/image/ab67616d00001e022e2a9617a5b479b2f8bcd4a4",
+    artists: [
+      {
+        name: "ARTY",
+        url: "https://open.spotify.com/artist/1rSGNXhhYuWoq9BEz5DZGO",
+      },
+      {
+        name: "Haska",
+        url: "https://open.spotify.com/artist/0oNrNJk1WEVKDpbz4bw54T",
+      },
+      {
+        name: "Jay Sorrow",
+        url: "https://open.spotify.com/artist/3CjbN6OZLjg1pbfqTlSVB9",
+      },
+    ],
+  },
+  {
+    title: "Like You Love Me",
+    songUrl: "https://open.spotify.com/track/3LYXjnYXAgDmhawWjl5spw",
+    albumImageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02037dd6244f9f3741537e0df2",
+    artists: [
+      {
+        name: "Conro",
+        url: "https://open.spotify.com/artist/1BAdSa5cdtCNLbvT7gWmtJ",
+      },
+      {
+        name: "Disero",
+        url: "https://open.spotify.com/artist/0YVFdCm1S3ikQU0yl110lu",
+      },
+      {
+        name: "ALICE FRANCE",
+        url: "https://open.spotify.com/artist/7BZmPyQJSf1ais5n7Icau2",
+      },
+    ],
+  },
+  {
+    title: "INOXENTE - Steve Aoki & Dee Mad Remix",
+    songUrl: "https://open.spotify.com/track/6aOGTQ6hMqe39KUCDy0drM",
+    albumImageUrl:
+      "https://i.scdn.co/image/ab67616d00001e028d5ae8f1941d8edb73ba9ecc",
+    artists: [
+      {
+        name: "Andrekza",
+        url: "https://open.spotify.com/artist/7K2ZrWY8iteGlM7G4V9B0s",
+      },
+      {
+        name: "Steve Aoki",
+        url: "https://open.spotify.com/artist/77AiFEVeAVj2ORpC85QVJs",
+      },
+      {
+        name: "Dee Mad",
+        url: "https://open.spotify.com/artist/6ofHQpykLy6SINDBBKqoOI",
+      },
+    ],
+  },
+  {
+    title: "Done With Love",
+    songUrl: "https://open.spotify.com/track/0qlAureWHln3vDCsqnkyRE",
+    albumImageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02d040bc5b46915b49a64d7b1d",
+    artists: [
+      {
+        name: "Zedd",
+        url: "https://open.spotify.com/artist/2qxJFvFYMEDqd7ui6kSAcq",
+      },
+    ],
+  },
+  {
+    title: "See The End",
+    songUrl: "https://open.spotify.com/track/1dDDVfAfms7M6VLLKmubaJ",
+    albumImageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02636c7aa52756542a618aa725",
+    artists: [
+      {
+        name: "Above & Beyond",
+        url: "https://open.spotify.com/artist/10gzBoINW3cLJfZUka8Zoe",
+      },
+      {
+        name: "Seven Lions",
+        url: "https://open.spotify.com/artist/6fcTRFpz0yH79qSKfof7lp",
+      },
+      {
+        name: "Opposite the Other",
+        url: "https://open.spotify.com/artist/3YPzVOVeAXdzhmnffu1WjZ",
+      },
+    ],
+  },
+  {
+    title: "Feels Just Like It - LP Giobbi Club Edit",
+    songUrl: "https://open.spotify.com/track/5hhPKIYauZTTHflcd7x3ac",
+    albumImageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02868f9f46dbce9c52024448d4",
+    artists: [
+      {
+        name: "LP Giobbi",
+        url: "https://open.spotify.com/artist/3oKnyRhYWzNsTiss5n4Z1J",
+      },
+      {
+        name: "Caroline Byrne",
+        url: "https://open.spotify.com/artist/2tVd9Bpt5Li9UsmKwhJ1nG",
+      },
+    ],
+  },
+  {
+    title: "What's Done Is Done",
+    songUrl: "https://open.spotify.com/track/6QIKDNjHrpQeqVtLvrdWNu",
+    albumImageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02b2c31f859828233e40aa93c7",
+    artists: [
+      {
+        name: "Seven Lions",
+        url: "https://open.spotify.com/artist/6fcTRFpz0yH79qSKfof7lp",
+      },
+      {
+        name: "HALIENE",
+        url: "https://open.spotify.com/artist/1sKIizVYeHkGy7Tjmn9QRj",
+      },
+    ],
+  },
+  {
+    title: "Hide and Seek - Le Youth Remix",
+    songUrl: "https://open.spotify.com/track/6uSLLJJ5r936DO1SASSViy",
+    albumImageUrl:
+      "https://i.scdn.co/image/ab67616d00001e02ac2c4a5d1938b34c2e2c1406",
+    artists: [
+      {
+        name: "Catie Turner",
+        url: "https://open.spotify.com/artist/3nYYI90ObxhjLjdxaoXGSa",
+      },
+      {
+        name: "Le Youth",
+        url: "https://open.spotify.com/artist/1Zz6NBe8UIZjm88TvehFtx",
+      },
+    ],
+  },
+];
+
+export const fallbackArtists = [
+  {
+    name: "Above & Beyond",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616100005174dc46ca82234955fec2bef92f",
+    artistUrl: "https://open.spotify.com/artist/10gzBoINW3cLJfZUka8Zoe",
+  },
+  {
+    name: "Seven Lions",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616100005174c214a46c2e8e846c6e94633c",
+    artistUrl: "https://open.spotify.com/artist/6fcTRFpz0yH79qSKfof7lp",
+  },
+  {
+    name: "Tritonal",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616100005174b44e5d61a4fd3ccc65045fc7",
+    artistUrl: "https://open.spotify.com/artist/521qvhdobR0GzhvU6TFw76",
+  },
+  {
+    name: "Calvin Harris",
+    imageUrl:
+      "https://i.scdn.co/image/ab676161000051741e4bcd2bef1896648762dd6b",
+    artistUrl: "https://open.spotify.com/artist/7CajNmpbOovFoOoasH2HaY",
+  },
+  {
+    name: "Charlotte de Witte",
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000517486c925d4853c9d09c6591d64",
+    artistUrl: "https://open.spotify.com/artist/1lJhME1ZpzsEa5M0wW6Mso",
+  },
+  {
+    name: "ARTY",
+    imageUrl:
+      "https://i.scdn.co/image/ab676161000051744fcd0525e000440ef442dfff",
+    artistUrl: "https://open.spotify.com/artist/1rSGNXhhYuWoq9BEz5DZGO",
+  },
+  {
+    name: "Zedd",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616100005174e28762aed82cde1178fb3873",
+    artistUrl: "https://open.spotify.com/artist/2qxJFvFYMEDqd7ui6kSAcq",
+  },
+  {
+    name: "RÜFÜS DU SOL",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616100005174dbe0d58f538b7d18e324af5e",
+    artistUrl: "https://open.spotify.com/artist/5Pb27ujIyYb33zBqVysBkj",
+  },
+  {
+    name: "Armin van Buuren",
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000517490db664ba1bf6574239f859d",
+    artistUrl: "https://open.spotify.com/artist/0SfsnGyD8FpIN4U4WCkBZ5",
+  },
+  {
+    name: "Tiësto",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616100005174e84e08fb1dfa2bf9b5a61563",
+    artistUrl: "https://open.spotify.com/artist/2o5jDhtHVPhrJdv3cEQ99Z",
+  },
+];
+
+export const fallbackNowPlaying = {
+  isPlaying: false,
+  title: "Not Playing",
+  artist: "Unknown Artist",
+  album: "Unknown Album",
+  albumImageUrl:
+    "https://i.scdn.co/image/ab67616d0000b2732a038d3bf875d23e4aeaa84e",
+  songUrl: "https://open.spotify.com",
+};
