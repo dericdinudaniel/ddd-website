1a663419d9f231fcc35518b81b0fbf2b6688d2aa
Deric Dinu Daniel <dericdd@umich.edu>
2025-05-11 21:10:48 +0530

    add custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import Section from "@/components/Sections/Section";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <Section>
+      <h1 className="font-bold font-header tracking-[.1rem] text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+        ERROR 404: NOT FOUND
+      </h1>
+    </Section>
+  );
+};
+
+export default NotFound;
